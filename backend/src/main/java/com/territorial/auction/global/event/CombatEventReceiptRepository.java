8fbce0d26c313a68e3ae9bbbfd17af2b749af53e
Kovalt03 <kalswp2003@naver.com>
2026-09-02 23:49:30 +0900

    [FEAT][infra] combat 계약 멱등 저장소 추가

---
+++ backend/src/main/java/com/territorial/auction/global/event/CombatEventReceiptRepository.java
@@ -0,0 +1,5 @@
+package com.territorial.auction.global.event;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CombatEventReceiptRepository extends JpaRepository<CombatEventReceipt, String> {}
